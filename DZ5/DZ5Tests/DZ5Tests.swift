4674f15ca6776e69ea0264c49f8b0cb7636ab3c3
Irina <irenmast@gmail.com>
2018-05-07 19:41:41 +0300

    1

---
+++ DZ5/DZ5Tests/DZ5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  DZ5Tests.swift
+//  DZ5Tests
+//
+//  Created by Ирина on 07.05.2018.
+//  Copyright © 2018 Ирина. All rights reserved.
+//
+
+import XCTest
+@testable import DZ5
+
+class DZ5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
